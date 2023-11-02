9418d9a917d160518fa23dfd847f16e0e14425cb
JumDest <juanmaloaizagarcia@gmail.com>
2023-11-01 22:40:26 -0500

    Se crean los atributos para la tabla loans

--- database/migrations/2023_10_31_224530_create_loans_table.php
+++ database/migrations/2023_10_31_224530_create_loans_table.php
@@ -14,7 +14,13 @@ class CreateLoansTable extends Migration
     public function up()
     {
         Schema::create('loans', function (Blueprint $table) {
-            $table->id();
+            $table->id()->comment("Identificador del Prestamo");
+            $table->unsignedBigInteger("id_contra")->comment("Id del Contratista");
+            $table->foreign("id_contra")->references("id")->on("contractors");
+            $table->unsignedBigInteger("id_team")->comment("Id del Equipo de Campo");
+            $table->foreign("id_team")->references("id")->on("equipment");
+            $table->date("date_dev")->comment("Fecha de Devolución");
+            $table->date("date_prest")->comment("Fecha de Prestamo");
             $table->timestamps();
         });
     }
