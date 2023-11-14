f2caff1d895abc4551b9ba25c08d96faca2c5d3a
davmorenoga <davmorenoga@unal.edu.co>
2023-11-14 10:20:48 -0500

    Se agregan el codigo necesario para generar un reporte en excel

--- resources/views/equipment/gestion_equipments.blade.php
+++ resources/views/equipment/gestion_equipments.blade.php
@@ -98,6 +98,7 @@
                         @endforeach
                     </tbody>
                 </table>
+                <button onclick="generarReporte()" class="btn btn-primary">Exportar a Excel</button>
             </div>
         </div>
                     <footer>
@@ -121,6 +122,8 @@
                     </footer>
                 </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
+<script src="{{ asset('/js/excel.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
