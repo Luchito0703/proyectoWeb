c99a0b6b940f08f753bb3914ae72acb24c4d73d9
JumDest <juanmaloaizagarcia@gmail.com>
2023-11-14 18:40:49 -0500

    Se implementan gráficos y PDF al sistema

--- resources/views/certificado/gestion_certificate.blade.php
+++ resources/views/certificado/gestion_certificate.blade.php
@@ -123,8 +123,7 @@
                                 <input type="date" class="form-control text-center" name="date_expedition" id="dateExp" >
 
                             </div>
-
-                            <button type="submit" class="btn btn-success mt-3">Generar certificado</button>
+                            <button type="submit" id="generateCertificateBtn" class="btn btn-success mt-2">Generar certificado</button>
                         </form>
                     </div>
                 </div>
@@ -151,6 +150,10 @@
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="/js/pdf.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+    <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+
 </body>
 </html>
 
--- resources/views/contrato/gestion_contract.blade.php
+++ resources/views/contrato/gestion_contract.blade.php
@@ -140,6 +140,7 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <a href="/HTML/graphic_contract.html" target="_blank"><button class="btn btn-info">Mostrar Gráfico</button></a>
                     </div>
                 </div>
             </div>
@@ -167,5 +168,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+    <script src="/js/graphic_contract.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 </body>
 </html>
