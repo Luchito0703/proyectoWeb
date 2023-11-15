8d81a3e7bc8a6999e27ea0d3df2b150def952fc0
Luis <lulopezu@unal.edu.co>
2023-11-14 21:47:11 -0500

    Se realizan pequeños cambios esteticos y en la funcionalidad de un boton

--- resources/views/loan/gestion_loan.blade.php
+++ resources/views/loan/gestion_loan.blade.php
@@ -168,6 +168,9 @@
             </div>
         </div>
     </footer>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
